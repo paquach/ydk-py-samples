0865cab3422ac7710fcf69576859cec72d554298
Santiago Alvarez <saalvare@cisco.com>
2017-06-01 10:12:33 -0700

    Add Codec boilerplate apps for all XR models

---
+++ samples/basic/codec/models/cisco-ios-xr/Cisco-IOS-XR-icpe-infra-cfg/cd-encode-xr-icpe-infra-cfg-10-ydk.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+#
+# Copyright 2016 Cisco Systems, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""
+Encode configuration for model Cisco-IOS-XR-icpe-infra-cfg.
+
+usage: cd-encode-xr-icpe-infra-cfg-10-ydk.py [-h] [-v]
+
+optional arguments:
+  -h, --help     show this help message and exit
+  -v, --verbose  print debugging messages
+"""
+
+from argparse import ArgumentParser
+from urlparse import urlparse
+
+from ydk.services import CodecService
+from ydk.providers import CodecServiceProvider
+from ydk.models.cisco_ios_xr import Cisco_IOS_XR_icpe_infra_cfg \
+    as xr_icpe_infra_cfg
+import logging
+
+
+def config_nv_satellite_global(nv_satellite_global):
+    """Add config data to nv_satellite_global object."""
+    pass
+
+
+if __name__ == "__main__":
+    """Execute main program."""
+    parser = ArgumentParser()
+    parser.add_argument("-v", "--verbose", help="print debugging messages",
+                        action="store_true")
+    args = parser.parse_args()
+
+    # log debug messages if verbose argument specified
+    if args.verbose:
+        logger = logging.getLogger("ydk")
+        logger.setLevel(logging.DEBUG)
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(("%(asctime)s - %(name)s - "
+                                      "%(levelname)s - %(message)s"))
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    # create codec provider
+    provider = CodecServiceProvider(type="xml")
+
+    # create codec service
+    codec = CodecService()
+
+    nv_satellite_global = xr_icpe_infra_cfg.NvSatelliteGlobal()  # create object
+    config_nv_satellite_global(nv_satellite_global)  # add object configuration
+
+    # encode and print object
+    # print(codec.encode(provider, nv_satellite_global))
+
+    provider.close()
+    exit()
+# End of script
---
+++ samples/basic/codec/models/cisco-ios-xr/Cisco-IOS-XR-ppp-ma-syslog-cfg/cd-encode-xr-ppp-ma-syslog-cfg-10-ydk.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+#
+# Copyright 2016 Cisco Systems, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""
+Encode configuration for model Cisco-IOS-XR-ppp-ma-syslog-cfg.
+
+usage: cd-encode-xr-ppp-ma-syslog-cfg-10-ydk.py [-h] [-v]
+
+optional arguments:
+  -h, --help     show this help message and exit
+  -v, --verbose  print debugging messages
+"""
+
+from argparse import ArgumentParser
+from urlparse import urlparse
+
+from ydk.services import CodecService
+from ydk.providers import CodecServiceProvider
+from ydk.models.cisco_ios_xr import Cisco_IOS_XR_ppp_ma_syslog_cfg \
+    as xr_ppp_ma_syslog_cfg
+import logging
+
+
+def config_ppp(ppp):
+    """Add config data to ppp object."""
+    pass
+
+
+if __name__ == "__main__":
+    """Execute main program."""
+    parser = ArgumentParser()
+    parser.add_argument("-v", "--verbose", help="print debugging messages",
+                        action="store_true")
+    args = parser.parse_args()
+
+    # log debug messages if verbose argument specified
+    if args.verbose:
+        logger = logging.getLogger("ydk")
+        logger.setLevel(logging.DEBUG)
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(("%(asctime)s - %(name)s - "
+                                      "%(levelname)s - %(message)s"))
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    # create codec provider
+    provider = CodecServiceProvider(type="xml")
+
+    # create codec service
+    codec = CodecService()
+
+    ppp = xr_ppp_ma_syslog_cfg.Ppp()  # create object
+    config_ppp(ppp)  # add object configuration
+
+    # encode and print object
+    # print(codec.encode(provider, ppp))
+
+    provider.close()
+    exit()
+# End of script
